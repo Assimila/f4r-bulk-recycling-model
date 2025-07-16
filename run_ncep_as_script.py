d7d31f4226fedf73ee12217dbb13bc3f43200ac4
Oxford BDS Coalition <bds.coalition.oxford@proton.me>
2025-07-16 16:30:30 +0100

    stashed_changes

--- run_ncep_as_script.py
+++ run_ncep_as_script.py
@@ -21,7 +21,7 @@ def latlonlev(dataset_list,latmax,latmin,lonmax,lonmin,pmin,pmax,deg):
     dataset_out = []
     for d in dataset_list:
 
-        d = d.sel(time=slice("1980-01-01", "2005-12-31"),drop=True)
+        d = d.sel(time=slice("1990-03-01", "1990-06-01"),drop=True)
 
         # make sure lat runs from south to north
         if not d["lat"].to_index().is_monotonic_increasing:
@@ -67,7 +67,7 @@ ds_uwnd  = xr.open_dataset("/Users/ellendyer/Library/Mobile Documents/com~apple~
 ds_vwnd = xr.open_dataset("/Users/ellendyer/Library/Mobile Documents/com~apple~CloudDocs/1SHARED_WORK/Work/3_ESA_GRANT/MODEL/data/ncep_test/vwnd.mon.mean.nc")
 ds_list_in = [ds_prate.rename({"prate": "Prec"}), ds_pres.rename({"pres": "Psfc"}), ds_lhtfl.rename({"lhtfl": "Evap"}), 
                ds_shum.rename({"shum": "Shum"}), ds_uwnd.rename({"uwnd": "Uwnd"}), ds_vwnd.rename({"vwnd": "Vwnd"})] 
-ds_list_out = latlonlev(ds_list_in,latmin=-10,latmax=10,lonmin=11,lonmax=31,pmax=1013,pmin=300,deg=2.5)
+ds_list_out = latlonlev(ds_list_in,latmin=-10,latmax=10,lonmin=11,lonmax=31,pmax=1000,pmin=300,deg=2.5)
 ds = xr.merge(ds_list_out) 
 print('datasets merged')
 
@@ -75,8 +75,8 @@ print('datasets merged')
 #ds = ds.sel(lat=slice(-12, 5), lon=slice(15, 25))
 # %%
 # grab the first time step
-#ds = ds.sel(time='1990-04-01', drop=True)
-ds = ds.sel(time=slice('1990-01-01','1990-12-31'), drop=True)
+ds = ds.sel(time='1990-04-01', drop=True)
+#ds = ds.sel(time=slice('1990-04-01','1990-07-01'), drop=True)
 print(ds)
 
 # %%
@@ -108,31 +108,12 @@ def integrator(a: np.ndarray, axis: int, x: np.ndarray) -> np.ndarray:
         x=x,
     )
 
-def surf_ext(ps,data):
-    print(ps)
-    print(data.level.max())
-
 # %%
 print('prepping datasets near surface for recycling')
-<<<<<<< Updated upstream
-from bulk_recycling_model import numerical_integration
-
-# Integrate 10^-3 Shum Uwnd dp
-# Because the integration limits are from high pressure to low pressure, we need to invert the sign.
-integrand = -1 * 1e-3 * ds["Shum"] * ds["Uwnd"]
-Fx = bulk_recycling_model.numerical_integration.integrate_no_extrapolation(integrand, ds["Psfc"])
-# Units: mb x m/s
-
-# Integrate 10^-3 Shum Vwnd dp
-# Because the integration limits are from high pressure to low pressure, we need to invert the sign.
-integrand = -1 * 1e-3 * ds["Shum"] * ds["Vwnd"]
-Fy = bulk_recycling_model.numerical_integration.integrate_no_extrapolation(integrand, ds["Psfc"])
-=======
 # Integrate 10^-3 Shum Uwnd dp
 # The input dataset has NaNs where pressure levels correspond to heights below ground level.
 # Because the integration limits are from high pressure to low pressure, we need to invert the sign.
 da = -1 * 1e-3 * ds["Shum"] * ds["Uwnd"]
-da = surf_ext(ds["Psfc"],da)
 Fx = da.reduce(integrator, dim="level", x=ds.coords["level"].values)
 # Units: mb x m/s
 
@@ -142,7 +123,6 @@ Fx = da.reduce(integrator, dim="level", x=ds.coords["level"].values)
 # Because the integration limits are from high pressure to low pressure, we need to invert the sign.
 da = -1 * 1e-3 * ds["Shum"] * ds["Vwnd"]
 Fy = da.reduce(integrator, dim="level", x=ds.coords["level"].values)
->>>>>>> Stashed changes
 # Units: mb x m/s
 
 # %% [markdown]
@@ -207,102 +187,100 @@ Fx = scaling.water_vapor_flux.convert(Fx.values, UnitSystem.natural, UnitSystem.
 Fy = scaling.water_vapor_flux.convert(Fy.values, UnitSystem.natural, UnitSystem.scaled)
 
 # %%
+# preprocess water vapor fluxes onto the secondary grid
+Fx_left = preprocess.prepare_Fx_left(Fx)
+Fx_right = preprocess.prepare_Fx_right(Fx)
+Fy_bottom = preprocess.prepare_Fy_bottom(Fy)
+Fy_top = preprocess.prepare_Fy_top(Fy)
+
+# %%
 # convert E to scaled units
 print('pre-scaled',ds['Evap'])
 E = scaling.evaporation.convert(ds["Evap"].values, UnitSystem.natural, UnitSystem.scaled)
 print('scaled',E)
-print('Fx',Fx)
 
-# Entering preprocessing and time step loop
-for i,time in enumerate(ds.time):
-    print(i,time.values)    
-     
-    # %%
-    # preprocess E onto the secondary grid
-    Ei = preprocess.prepare_E(E[:,:,i])
-    
-    # %%
-    # preprocess water vapor fluxes onto the secondary grid
-    Fxi_left = preprocess.prepare_Fx_left(Fx[:,:,i])
-    Fxi_right = preprocess.prepare_Fx_right(Fx[:,:,i])
-    Fyi_bottom = preprocess.prepare_Fy_bottom(Fy[:,:,i])
-    Fyi_top = preprocess.prepare_Fy_top(Fy[:,:,i])
-    
-    # %%
-    # compute P
-    Pi = preprocess.calculate_precipitation(Fxi_left, Fxi_right, Fyi_bottom, Fyi_top, Ei, dx, dy)
-    
-    # %% [markdown]
-    # Run the model
-    
-    # %%
-    import matplotlib.pyplot as plt
-    
-    # %%
-    import logging
-    
-    logging.basicConfig()
-    logging.getLogger("bulk_recycling_model").setLevel(logging.INFO)
-    
-    # %%
-    print("run model and plot")
-    from bulk_recycling_model import plotting
-    from bulk_recycling_model.main import run
-    
-    # %%
-    class Callback:
-        def __init__(self):
-            self.n_calls = 0  # number of times callback has executed
-    
-        def __call__(self, rho: np.ndarray, k: int):
-            fig, ax = plt.subplots()
-            collection = plotting.pcolormesh(ax, rho, lon_axis, lat_axis, vmin=0.0, vmax=0.8)
-            fig.colorbar(collection)
-            fig.suptitle(f"$\\rho^k$ @ k={k:04d}")
-            plt.savefig(f"/Users/ellendyer/Library/Mobile Documents/com~apple~CloudDocs/1SHARED_WORK/Work/3_ESA_GRANT/MODEL/plots/ncep/{self.n_calls:05d}_{k:04d}.png")
-            plt.close(fig)
-            self.n_calls += 1
-    
-    # %%
-    #! rm plots/*.png
-    
-    # %%
-    status = run(
-        Fxi_left,
-        Fxi_right,
-        Fyi_bottom,
-        Fyi_top,
-        Ei,
-        Pi,
-        dx,
-        dy,
-        tol=1e-3,
-        #callback=Callback(),
-    )
-    assert status["success"]
-    print(time.values)
+# %%
+# preprocess E onto the secondary grid
+E = preprocess.prepare_E(E)
 
-    # %%
-    # plot each timestep 
-    
-    rho = status["rho"]
-    fig, ax = plt.subplots()
-    collection = plotting.pcolormesh(ax, rho, lon_axis, lat_axis, vmin=0.0, vmax=0.8)
-    fig.colorbar(collection)
-    fig.suptitle(str(time.values)+" $\\rho$")
-    plt.savefig(f"/Users/ellendyer/Library/Mobile Documents/com~apple~CloudDocs/1SHARED_WORK/Work/3_ESA_GRANT/MODEL/plots/ncep/rho_"+str(time.values)+".png")
-    plt.show()
-    
-    # %%
-    # plot the convergence
-    deltas = status["deltas"]
-    fig, ax = plt.subplots()
-    ax.plot(deltas)
-    ax.set_title("Convergence")
-    ax.set_xlabel("Iteration")
-    plt.show()
-    # %%
-    
-    
-    
-    
+# %%
+# compute P
+P = preprocess.calculate_precipitation(Fx_left, Fx_right, Fy_bottom, Fy_top, E, dx, dy)
+
+
+# %% [markdown]
+# Run the model
+
+# %%
+import matplotlib.pyplot as plt
+
+# %%
+import logging
+
+logging.basicConfig()
+logging.getLogger("bulk_recycling_model").setLevel(logging.INFO)
+
+# %%
+print("run model and plot")
+from bulk_recycling_model import plotting
+from bulk_recycling_model.main import run
+
+# %%
+class Callback:
+    def __init__(self):
+        self.n_calls = 0  # number of times callback has executed
+
+    def __call__(self, rho: np.ndarray, k: int):
+        fig, ax = plt.subplots()
+        collection = plotting.pcolormesh(ax, rho, lon_axis, lat_axis, vmin=0.0, vmax=0.8)
+        fig.colorbar(collection)
+        fig.suptitle(f"$\\rho^k$ @ k={k:04d}")
+        plt.savefig(f"/Users/ellendyer/Library/Mobile Documents/com~apple~CloudDocs/1SHARED_WORK/Work/3_ESA_GRANT/MODEL/plots/ncep/{self.n_calls:05d}_{k:04d}.png")
+        plt.close(fig)
+        self.n_calls += 1
+
+# %%
+#! rm plots/*.png
+
+# %%
+status = run(
+    Fx_left,
+    Fx_right,
+    Fy_bottom,
+    Fy_top,
+    E,
+    P,
+    dx,
+    dy,
+    tol=1e-2,
+    callback=Callback(),
+)
+assert status["success"]
+
+# %% [markdown]
+# Animate the plots using ffmpeg
+# 
+# ```bash
+# ffmpeg -framerate 30 -pattern_type glob -i "*.png" -vf "tpad=stop_mode=clone:stop_duration=3" -c:v libx264 -pix_fmt yuv420p mov.mp4
+# ```
+
+# %%
+# plot the final solution
+rho = status["rho"]
+fig, ax = plt.subplots()
+collection = plotting.pcolormesh(ax, rho, lon_axis, lat_axis, vmin=0.0, vmax=0.8)
+fig.colorbar(collection)
+fig.suptitle("$\\rho$")
+plt.show()
+# %%
+# plot the convergence
+deltas = status["deltas"]
+fig, ax = plt.subplots()
+ax.plot(deltas)
+ax.set_title("Convergence")
+ax.set_xlabel("Iteration")
+plt.show()
+# %%
+
+
+
