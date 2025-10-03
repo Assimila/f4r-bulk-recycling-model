0b17825061212cf00c87ef08749a2978185800f1
ellendyer <ellendyer.ubuhoro@gmail.com>
2025-10-03 17:03:57 +0100

    3Oct_a

---
+++ bulk_recycling_model/main_orig.py
@@ -0,0 +1,314 @@
+import logging
+from datetime import UTC, datetime, timedelta
+from typing import Callable, TypedDict
+
+import numpy as np
+
+from .coefficients import Coefficients
+from .utils import (
+    check_lr_flux,
+    check_tb_flux,
+    diagonal,
+    drop_first,
+    drop_last,
+    inflow_mask,
+    outflow_mask,
+    unbuffer,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class RunStatus(TypedDict):
+    success: bool
+
+    # solution for rho on the secondary grid.
+    # shape (N, M)
+    # N = number of points in longitude.
+    # M = number of points in latitude.
+    rho: np.ndarray
+
+    # number of iterations
+    k: int
+
+    # delta per iteration
+    deltas: list[float]
+
+    # time taken
+    time_taken: timedelta
+
+
+def callback(rho: np.ndarray, k: int) -> None:
+    """
+    Noop callback function.
+
+    Args:
+        rho: the auxiliary variable rho, with shape (N, M) on the secondary grid.
+            N = number of points in longitude.
+            M = number of points in latitude.
+        k: the iteration number
+    """
+    pass
+
+
+def run(
+    Fx_left: np.ndarray,
+    Fx_right: np.ndarray,
+    Fy_bottom: np.ndarray,
+    Fy_top: np.ndarray,
+    E: np.ndarray,
+    P: np.ndarray,
+    dx: float,
+    dy: float,
+    rho_0: float = 0.0,
+    R: float = 0.2,
+    R_1: float = 0.2,
+    max_iter: int = 1000,
+    tol: float = 1e-3,
+    callback: Callable[[np.ndarray, int], None] = callback,
+) -> RunStatus:
+    """
+    Solve for the auxiliary variable rho of the bulk recycling model.
+
+    Units: all variables must be provided in scaled units.
+
+    Inputs and output should have shape (N, M) on the secondary grid.
+    N = number of points in longitude.
+    M = number of points in latitude.
+
+    Args:
+        Fx_left: longitudinal water vapor flux on the left hand side of each cell
+        Fx_right: longitudinal water vapor flux on the right hand side of each cell
+        Fy_bottom: latitudinal water vapor flux on the bottom side of each cell
+        Fy_top: latitudinal water vapor flux on the top side of each cell
+        E: evaporation
+        P: precipitation
+        dx: longitudinal grid spacing
+        dy: latitudinal grid spacing
+        rho_0: initial guess for the auxiliary variable rho
+        R: relaxation parameter within the model domain
+        R_1: relaxation parameter external to the model domain.
+            used for extrapolation of outflows.
+        max_iter: maximum number of iterations
+        tol: tolerance for convergence.
+            The algorithm halts when the absolute difference between iterations (including relaxation) is less than tol.
+        callback: A callback function, with arguments (rho, k).
+            May be called multiple times per iteration.
+
+    Raises:
+        ValueError: if any of the inputs are not valid
+        RuntimeError: if the algorithm does not converge within max_iter iterations
+    """
+    # ------------------------------------------------------------------------------------------------------------------
+    # Validate inputs
+    # ------------------------------------------------------------------------------------------------------------------
+
+    if E.ndim != 2:
+        raise ValueError("array must be 2D")
+
+    (N, M) = E.shape
+
+    if N < 2 or M < 2:
+        # for boundary conditions to work
+        raise ValueError("array too small")
+
+    # check all arrays the same shape
+    for arr in [Fx_left, Fx_right, Fy_bottom, Fy_top, E, P]:
+        if arr.shape != (N, M):
+            raise ValueError("arrays must be the same shape")
+
+    for arr in [Fx_left, Fx_right, Fy_bottom, Fy_top, E, P]:
+        if not np.isfinite(arr).all():
+            raise ValueError("array values must be finite")
+
+    check_lr_flux(Fx_left, Fx_right)
+    check_tb_flux(Fy_top, Fy_bottom)
+
+    # check dx, dy > 0
+    if dx <= 0 or dy <= 0:
+        raise ValueError("dx and dy must be positive")
+
+    # check 0 <= rho_0 < 1
+    if not (0 <= rho_0 < 1):
+        raise ValueError("rho_0 must be between 0 and 1")
+
+    # check 0 < R, R_1 <= 1
+    if not (0 < R <= 1 and 0 < R_1 <= 1):
+        raise ValueError("R and R_1 must be between 0 and 1")
+
+    # max_iter > 0
+    if max_iter <= 0:
+        raise ValueError("max_iter must be positive")
+
+    # check tol > 0
+    if tol <= 0:
+        raise ValueError("tol must be positive")
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # Prepare for iteration
+    # ------------------------------------------------------------------------------------------------------------------
+
+    # Create coefficients object
+    coeffs = Coefficients(Fx_left, Fx_right, Fy_bottom, Fy_top, E, P, dx, dy)
+
+    # create mask of grid cells external to the model domain (on buffered secondary grid)
+    m_outflow = outflow_mask(Fx_left, Fx_right, Fy_bottom, Fy_top)
+    m_inflow = inflow_mask(Fx_left, Fx_right, Fy_bottom, Fy_top)
+
+    # shape of the buffered secondary grid
+    (N_buffered, M_buffered) = (N + 2, M + 2)
+
+    # initial guess for rho (on buffered secondary grid)
+    rho = np.full((N_buffered, M_buffered), rho_0, dtype=float)
+    # in the buffered region external to the model domain, set zeros in place of inflows.
+    # this is cosmetic, as these cells are not used.
+    rho[m_inflow] = 0.0
+
+    # keep track of the delta change per iteration
+    deltas = []
+    
+    # loop variables
+    k = 0
+    iter_start = datetime.now(UTC)
+    time_taken = timedelta(0)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # Iterate
+    # ------------------------------------------------------------------------------------------------------------------
+
+    for k in range(1, max_iter + 1):
+        # --------------------------------------------------------------------------------------------------------------
+        # Take a step
+        # --------------------------------------------------------------------------------------------------------------
+
+        step_start = datetime.now(UTC)
+
+        rho_next = rho.copy()
+
+        # This is a nice debug trick: fill rho_next with NaNs.
+        # This ensures that the implementation does not use k+1 data from
+        # cells where it does not yet exist.
+        # By the end of the iteration, no NaNs should be left.
+        # unbuffer(rho_next).fill(np.nan)
+
+        # --------------------------------------------------------------------------------------------------------------
+        # Solve for rho within the model domain
+        # --------------------------------------------------------------------------------------------------------------
+
+        logger.debug(f"Iteration {k} of {max_iter}. Solving for rho within the model domain")
+
+        # We must solve from the bottom left corner (lon/lat).
+        # Iterating by the diagonal is cosmetic if we go cell by cell, but may be faster if vectorized.
+        # TODO: try to vectorize the inner loop
+        # skip the first and last 2 diagonals on the buffered grid, as these are outside the model domain
+        for diag in range(-M_buffered + 3, N_buffered - 2):
+            # ----------------------------------------------------------------------------------------------------------
+            # Per diagonal
+            # ----------------------------------------------------------------------------------------------------------
+
+            logger.debug(f"Iteration {k} of {max_iter}, diagonal {diag}")
+
+            # skip the first and last cell of the diagonal - 
+            # these are in the buffer, outside the model domain, and updated later.
+            for i, j in drop_first(drop_last(diagonal(N_buffered, M_buffered, diag))):
+                # ------------------------------------------------------------------------------------------------------
+                # Per cell within the model domain
+                # ------------------------------------------------------------------------------------------------------
+
+                logger.debug(f"Iteration {k} of {max_iter}, diagonal {diag}, cell ({i}, {j})")
+
+                A_0 = coeffs.A_0_buffered[i, j]
+
+                A_1 = (
+                    coeffs.alpha_1_buffered[i, j]
+                    + coeffs.alpha_C_buffered[i, j] * rho[i, j]
+                    + coeffs.alpha_U_buffered[i, j] * rho[i, j + 1]
+                    + coeffs.alpha_R_buffered[i, j] * rho[i + 1, j]
+                    + coeffs.alpha_D_buffered[i, j] * rho_next[i, j - 1]
+                    + coeffs.alpha_L_buffered[i, j] * rho_next[i - 1, j]
+                )
+
+                # apply the relaxation parameter
+                rho_next[i, j] = rho[i, j] + R * (A_1 / A_0 - rho[i, j])
+
+            callback(rho_next, k)
+
+        # --------------------------------------------------------------------------------------------------------------
+        # Solve for external outflow cells by extrapolation
+        # --------------------------------------------------------------------------------------------------------------
+
+        logger.debug(f"Iteration {k} of {max_iter}. Solving for external outflow cells by extrapolation")
+
+        # left
+        i = 0
+        for j in range(M_buffered):
+            if m_outflow[i, j]:
+                rho_extr = 2 * rho_next[i + 1, j] - rho_next[i + 2, j]
+                # apply the relaxation parameter
+                rho_next[i, j] = rho[i, j] + R_1 * (rho_extr - rho[i, j])
+
+        # right
+        i = -1
+        for j in range(M_buffered):
+            if m_outflow[i, j]:
+                rho_extr = 2 * rho_next[i - 1, j] - rho_next[i - 2, j]
+                # apply the relaxation parameter
+                rho_next[i, j] = rho[i, j] + R_1 * (rho_extr - rho[i, j])
+
+        # bottom
+        j = 0
+        for i in range(N_buffered):
+            if m_outflow[i, j]:
+                rho_extr = 2 * rho_next[i, j + 1] - rho_next[i, j + 2]
+                # apply the relaxation parameter
+                rho_next[i, j] = rho[i, j] + R_1 * (rho_extr - rho[i, j])
+
+        # top
+        j = -1
+        for i in range(N_buffered):
+            if m_outflow[i, j]:
+                rho_extr = 2 * rho_next[i, j - 1] - rho_next[i, j - 2]
+                # apply the relaxation parameter
+                rho_next[i, j] = rho[i, j] + R_1 * (rho_extr - rho[i, j])
+
+        callback(rho_next, k)
+
+        # --------------------------------------------------------------------------------------------------------------
+        # Finished step
+        # --------------------------------------------------------------------------------------------------------------
+
+        #logger.info(
+        #    f"Iteration {k} of {max_iter}. Finished in {(datetime.now(UTC) - step_start).total_seconds()} seconds"
+        #)
+
+        delta = np.abs(unbuffer(rho_next) - unbuffer(rho)).max()
+        deltas.append(delta)
+        #logger.info(f"Iteration {k} of {max_iter}. delta = {delta}")
+
+        if delta < tol:
+            time_taken = datetime.now(UTC) - iter_start
+            logger.info(f"Converged in {k} iterations and {time_taken}")
+            return RunStatus(
+                success=True,
+                rho=unbuffer(rho_next),
+                k=k,
+                deltas=deltas,
+                time_taken=time_taken,
+            )
+
+        # update rho for next iteration
+        rho = rho_next
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # hit max_iter without converging
+    # ------------------------------------------------------------------------------------------------------------------
+
+    logger.warning(f"Did not converge in {max_iter} iterations")
+
+    return RunStatus(
+        success=False,
+        rho=unbuffer(rho),
+        k=k,
+        deltas=deltas,
+        time_taken=time_taken,
+    )
---
+++ merge_monthly_prepare_era5.py
@@ -0,0 +1,145 @@
+# %%
+import numpy as np
+import xarray as xr
+import pandas as pd
+import scipy
+import sys 
+import warnings
+import matplotlib.pyplot as plt
+from shapely.geometry import mapping
+import cartopy.crs as ccrs
+import cartopy.feature
+warnings.filterwarnings('ignore')
+import time as timer
+start_all = timer.time()
+
+# %%
+dataf ="/Volumes/ESA_F4R/era/" 
+datao ="/Volumes/ESA_F4R/ed_prepare/" 
+datap ="/Users/ellendyer/Library/Mobile Documents/com~apple~CloudDocs/1SHARED_WORK/Work/3_ESA_GRANT/MODEL/plots/era/"
+
+for YR in range(2008,2025):
+    print('Year: ',YR)
+    # %%
+    #For selection and plotting
+    time_bnds = (str(YR)+'-01-01',str(YR)+'-12-31')
+    lon_bnds, lat_bnds = (7, 35), (13,-16)
+    lon_bnds_f, lat_bnds_f = (9, 34), (-15,11) 
+    p_bnds = (30000,100000)
+    
+    # %% [markdown]
+    # **Read in ERA5 data on pressure levels (hourly timesteps in fortnightly files)**
+    # - *fortnightly files currently run from 1994-2024*
+    # - resampled to monthly MS timestep
+    # - shum multiplied by 1000 to convert from kg/kg --> g/kg
+    # - pressure levels are divided by 100 to convert from Pa to hPa (only for fortnightly files)
+    # - sort data by descending pressure levels (only for fortnightly files)
+    # 
+    # 
+    # **Input file units:**
+    # - plev - pa
+    # - q - kg/kg
+    # - u - m/s
+    # - v - m/s
+    
+    # %%
+    start = timer.time()
+    from functools import partial
+    def _preprocess_pres(x, lon_bnds, lat_bnds, p_bnds):
+        return x.sel(lon=slice(*lon_bnds), lat=slice(*lat_bnds),
+                     plev=slice(*p_bnds),drop=True)
+    partial_func_pres = partial(_preprocess_pres, lon_bnds=lon_bnds, lat_bnds=lat_bnds, p_bnds=p_bnds)
+    
+    #Reading in pressure level variables from ERA5
+    ds_era_pres = xr.open_mfdataset(dataf+"era5/pressure_levels/era5_pressure_level_variables_central_africa_"+str(YR)+"*.nc",
+                                    drop_variables=['r','t','w'],
+                                    preprocess=partial_func_pres,parallel=True).resample(time='MS').mean(dim='time').load()
+    ds_era_pres = ds_era_pres.rename({'plev':'level','q':'Shum','u':'Uwnd','v':'Vwnd'})
+    ds_era_pres['Shum'] = 1000.0*ds_era_pres['Shum']
+    ds_era_pres['level'] = ds_era_pres['level']/100.0  
+    ds_era_pres = ds_era_pres.sortby('level', ascending=False) 
+    end = timer.time()
+    length = end - start
+    print("ERA5 pressure level data read in took ", length, "seconds")
+    
+    # %% [markdown]
+    # **Read in ERA5 land data (hourly in monthly files)**
+    # - selecting hour 23 (0-23) of Prec and Evap because of how ERA5 Land variables are accumulated (https://confluence.ecmwf.int/pages/viewpage.action?pageId=197702790 - https://confluence.ecmwf.int/display/CKB/ERA5-Land%3A+data+documentation#ERA5Land:datadocumentation-accumulationsAccumulations)
+    # - prec is multiplied by 1000 to convert from m to mm
+    # - evap is multiplied by -1000 to convert from m to mm and upward fluxes in land model are considered negative
+    # - Prec, Evap, and Psfc are then resampled to MS monthly and also interpolated to coarser pressure level grid
+    # 
+    # **Input file units:**
+    # - tp - m
+    # - e - m (-)
+    # - sp - pa
+    
+    # %%
+    start = timer.time()
+    from functools import partial
+    def _preprocess_land(x, lon_bnds, lat_bnds):
+        x = x.sel(longitude=slice(*lon_bnds), latitude=slice(*lat_bnds),drop=True)
+        return x
+    partial_func_land = partial(_preprocess_land, lon_bnds=lon_bnds, lat_bnds=lat_bnds)
+    
+    #Reading in surface variables from ERA5 Land
+    ds_era_land = xr.open_mfdataset(dataf+"era5_land/era5_land_variables_central_africa_"+str(YR)+"*.nc",
+                                    drop_variables=['expver','number','pev','ssr','t2m'],
+                                    preprocess=partial_func_land,parallel=True).load()
+    ds_era_land = ds_era_land.rename({'valid_time':'time','latitude':'lat',
+                                      'longitude':'lon','tp':'Prec','e':'Evap','sp':'Psfc'})
+    ds_era_land = ds_era_land.interp(lat=ds_era_pres['lat'],lon=ds_era_pres['lon'],method='linear',kwargs={"fill_value": "extrapolate"})
+    Prec = ds_era_land['Prec'].where(ds_era_land['time.hour']==23,drop=True)*1000.0
+    Evap_land = ds_era_land['Evap'].where(ds_era_land['time.hour']==23,drop=True)*-1000.0
+    Prec = Prec.resample(time='MS').mean(dim='time') 
+    Evap_land = Evap_land.resample(time='MS').mean(dim='time')
+    Psfc_land = ds_era_land['Psfc'].resample(time='MS').mean(dim='time')/100.0
+    ds_era_land.close()
+    end = timer.time()
+    length = end - start
+    print("ERA5 land data read in took ", length, "seconds")
+    
+    # %% [markdown]
+    # Read in ERA5 surface vars for merging - setting non land evaporation to zero. Merging surface pressure level and land surface pressure to output. 
+    
+    # %%
+    start = timer.time()
+    
+    #Reading in surface variables from ERA5 surface files
+    ds_era_psfc = xr.open_mfdataset(dataf+"era5/era5_surface/era5_surface_pressure_central_africa_"+str(YR)+"*.nc",
+                                    drop_variables=['expver','number'],
+                                    preprocess=partial_func_land,parallel=True).load()
+    ds_era_psfc = ds_era_psfc.rename({'valid_time':'time','latitude':'lat',
+                                      'longitude':'lon','sp':'Psfc'})
+    ds_era_psfc = ds_era_psfc.interp(lat=ds_era_pres['lat'],lon=ds_era_pres['lon'],method='linear',kwargs={"fill_value": "extrapolate"})
+    Psfc_surface = ds_era_psfc['Psfc'].resample(time='MS').mean(dim='time')/100.0
+    ds_era_psfc.close()
+    
+    Evap = Evap_land.fillna(0.0)
+    Psfc = Psfc_land.combine_first(Psfc_surface)
+    
+    length = end - start
+    print("ERA5 surface data read in took ", length, "seconds")
+    
+    # %% [markdown]
+    # **Merging all input datasets into one dataset for recyling code called ds**
+    # - close both input datasets
+    # - sort everything so latitude is south to north
+    # - transpose dimensions so they run (lon,lat,level,time) as in recycling code
+    # - save input ds to file
+    
+    # %%
+    start = timer.time()
+    ds = xr.merge([ds_era_pres,Prec,Evap,Psfc]) 
+    ds_era_pres.close()
+    ds = ds.sortby('lat', ascending=True)
+    ds = ds.sel(lat=slice(*lat_bnds_f),lon=slice(*lon_bnds_f))
+    ds = ds.transpose("lon", "lat", "level", "time",missing_dims='ignore')
+    
+    ds.to_netcdf(datao+"merge_erads_"+str(YR)+".nc", mode='w', format='NETCDF4', engine='netcdf4')
+    end = timer.time()
+    length = end - start
+    print("Merging and dataset output took ", length, "seconds")
+    
+    
+    
