0b8b74ea7069f2e0b99e7e00c3270a1c64539603
ellendyer <64270734+ellendyer@users.noreply.github.com>
2026-05-05 16:46:26 +0100

    5May_a

--- bulk_recycling_model/numerical_integration.py
+++ bulk_recycling_model/numerical_integration.py
@@ -235,6 +235,8 @@ def integrate_with_extrapolation(
         },
     )
 
+    print(da)
+
     if check_finite:
         if not xr.ufuncs.isfinite(da).all():
             raise ValueError("Integration result is not finite")
